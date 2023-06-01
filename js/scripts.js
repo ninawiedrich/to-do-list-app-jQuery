0ffbbb6115c41e13472f70ba6397bbd39173b606
Nina Wiedrich <94755570+ninawiedrich@users.noreply.github.com>
2023-06-01 10:29:03 +0200

    Refactored code using jQuery for DOM manipulation and event handling

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function newItem() {
+  //Adding a new item to the list:
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+
+  //Crossing an item out:
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  //Adding a delete button
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  //Adding delete class on click
+  crossOutButton.on("click", deleteListItem);
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+
+  //Reordering the items:
+  $("#list").sortable();
+}
